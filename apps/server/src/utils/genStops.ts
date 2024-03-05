88b56a2bc2aff1907b32b5d463f03b7bba69ac69
Vicente <33096355+Vicente015@users.noreply.github.com>
2024-03-05 11:35:13 +0000

    refactor: :recycle: Improve genStop script combining data from GTFS and API

--- apps/server/src/utils/genStops.ts
+++ apps/server/src/utils/genStops.ts
@@ -1,47 +1,85 @@
 import { parse } from 'csv-parse/sync'
+import got from 'got'
 import fs from 'node:fs'
+import { z } from 'zod'
+import Constants from '../Constants.js'
 
-const googleTransitPath = (fileName: string) => `src/data/transit/${fileName}.csv`
+const TRANSIT_STOPS_PATH = 'src/data/transit/stops.csv'
 
-interface Stop {
-  stop_id: string
-  stop_code: number | undefined
-  stop_name: string
-  stop_desc: string | undefined
-  stop_lat: string
-  stop_lon: string
-  zone_id: string | undefined
-  stop_url: string | undefined
-  location_type: string | undefined
-  parent_station: string | undefined
-  stop_timezone: string | undefined
-  wheelchair_boarding: string | undefined
-}
-type Stops = Stop[]
+/**
+ * Esquema de la respuesta del listado de paradas de la API
+ */
+const APIStopsSchema = z.array(z.object({
+  nombre: z.string(),
+  numero: z.string()
+}))
 
-function genStopsJSON () {
-  // todo: sacar paradas de la api oficial porque así tienen menos acrónimos y nombres actualizados
-  const TRANSIT_STOPS_PATH = googleTransitPath('stops')
+/**
+ * Esquema de UNA parada en el archivo CSV de Google Transit
+ */
+const TransitStopSchema = z.object({
+  stop_id: z.string(),
+  stop_code: z.number().optional(),
+  stop_name: z.string(),
+  stop_desc: z.string().optional(),
+  stop_lat: z.string(),
+  stop_lon: z.string(),
+  zone_id: z.string().optional(),
+  stop_url: z.string().optional(),
+  location_type: z.string().optional(),
+  parent_station: z.string().optional(),
+  stop_timezone: z.string().optional(),
+  wheelchair_boarding: z.string().optional()
+})
+const TransitStopsSchema = z.array(TransitStopSchema)
+
+function getDataFromTransit () {
   const transitStringData = fs.readFileSync(TRANSIT_STOPS_PATH, { encoding: 'utf-8' })
-  const mapStop = (stop: Stop) => ({
+
+  const transitStops = parse(transitStringData, {
+    columns: true,
+    skip_empty_lines: true
+  }) as z.infer<typeof TransitStopsSchema>
+  const mapTransitStop = (stop: typeof transitStops[number]) => ({
     id: parseInt(stop.stop_id),
     name: stop.stop_name,
     latitude: stop.stop_lat,
     longitude: stop.stop_lon
   })
+  return transitStops.map(mapTransitStop)
+}
 
-  const transitCSVData = parse(transitStringData, {
-    columns: true,
-    skip_empty_lines: true
-  }) as Stops
-  const mappedData = transitCSVData
-    .map(mapStop)
-    .sort(((a, b) => a.id - b.id))
+async function getDataFromAPI () {
+  const APIStopsResponse = await got.get(`${Constants.API_URL}/paradas`, {
+    responseType: 'json',
+    headers: { accept: 'application/json' }
+  })
+  return APIStopsSchema.parse(APIStopsResponse.body)
+}
+
+async function genStopsJSON () {
+  let APIStops = await getDataFromAPI()
+  // Eliminar el primer elemento esquema de ejemplo
+  APIStops.shift()
+  const nextExampleElementIndex = APIStops.findIndex((stop) => stop.numero === 'PAR')
+  // Acorta la longitud del array
+  APIStops = APIStops.slice(0, nextExampleElementIndex)
+  const transitData = getDataFromTransit()
+
+  const finalData = APIStops.map(({ nombre, numero }) => {
+    const stopFoundInTransit = transitData.find(({ id }) => id === parseInt(numero))
+    return {
+      id: numero,
+      name: nombre,
+      latitude: stopFoundInTransit?.latitude,
+      longitude: stopFoundInTransit?.longitude
+    }
+  }).sort((a, b) => parseInt(a.id) - parseInt(b.id))
 
   fs.writeFileSync(
     'src/data/paradas.json',
-    JSON.stringify(mappedData, undefined, 2)
+    JSON.stringify(finalData, undefined, 2)
   )
 }
 
-export default genStopsJSON
+await genStopsJSON()
